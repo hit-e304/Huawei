e4d6ed2fb7f5ecb440f3c4b400d974349fbf75e6
VissageMo <434753117@qq.com>
2019-03-11 21:32:05 +0800

    graph_find_path

--- map.py
+++ map.py
@@ -24,3 +24,35 @@ cross = read_inf(cross_addr)
 road = read_inf(road_addr)
 car = read_inf(car_addr)
 
+def find_path(graph, start, end, path=[]):
+        path = path + [start]
+        if start == end:
+            return path
+        # if not graph.has_key(start):
+        #     return None
+        for node in graph[start]:
+            if node not in path:
+                newpath = find_path(graph, node, end, path)
+                if newpath: return newpath
+        return None
+
+def delete(li, index):
+    li_back = li[:index] + li[index+1:]
+    return li_back
+
+def map_graph(cross, road):
+    graph = {}
+    for i in range(len(cross)):
+        cross_i = cross[i]
+        cross_else = delete(cross, i)
+        connect_i = []
+
+        for j in cross_else:
+            for k in [1, 2, 3, 4]:
+                if j[k] in cross_i and j[k] != -1:
+                    connect_i.append(j[0])
+        
+        graph[cross_i[0]] = connect_i
+    return graph
+
+
